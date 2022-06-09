57288381237818e5a5f51d57bfe720bbc673b3bc
Shahin Alam <100690791+shahinalambd@users.noreply.github.com>
2022-06-09 12:26:20 +0600

    First Commit

---
+++ assets/js/customs.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+		
+
+		// Custom Js
+		AOS.init();
+	
+			
+		    $(window).scroll(function () {
+		        var scroll = $(window).scrollTop();
+		        // for top scroll
+		      	if (scroll >= 0) {
+		            $(".site-header").addClass("sticky");
+		        }
+		        if (scroll <= 0) {
+		            $(".site-header").removeClass("sticky");
+		        }
+		    });
+
+        // Scrollup
+        $.scrollUp({
+          scrollName: 'scrollUp', // Element ID
+          topDistance: '300', // Distance from top before showing element (px)
+          topSpeed: 300, // Speed back to top (ms)
+          animation: 'fade', // Fade, slide, none
+          animationInSpeed: 200, // Animation in speed (ms)
+          animationOutSpeed: 200, // Animation out speed (ms)
+          scrollText: '<i class="fa-solid fa-arrow-up"></i>', // Text for element
+          activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
+        });
+      
+       
+
+});
+
+
